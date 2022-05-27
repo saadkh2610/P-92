7a44def53d0393af68e0a6ec08fb07d30f5cc6da
saadkh2610 <92426237+saadkh2610@users.noreply.github.com>
2022-05-27 15:41:47 +0530

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,19 @@
+
+score = 0;
+
+function updateScore()
+{
+	score = score + 1;
+	document.getElementById("score").innerHTML = "Score: " + score;
+}
+
+function saveScore()
+{
+    localStorage.setItem("dbscore", score);
+}
+
+
+function nextPage() {
+window.location = "activity_2.html";
+
+}
